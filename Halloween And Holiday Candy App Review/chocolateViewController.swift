3e03bdb6afec497ac5d3ec169d5eb1a09310f0c0
Alexis Decker <deckera@hawaii.edu>
2023-11-02 13:21:15 -1000

    competed in class hackwich #9 part one through ten

--- Halloween And Holiday Candy App Review/chocolateViewController.swift
+++ Halloween And Holiday Candy App Review/chocolateViewController.swift
@@ -26,11 +26,47 @@ class chocolateViewController: UIViewController, UITableViewDataSource, UITableV
 
     }
     
+    //part 9
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "mySegue"
+                {
+                    let s1 = segue.destination as! secondDetailViewController
+                    let imageIndex = tableView.indexPathForSelectedRow?.row
+                    s1.imagePass = categoryOneImagesData[imageIndex!]
+                }
+            }
+
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+                {
+                    tableView.deselectRow(at: indexPath, animated: true)
+                }
+
+    
+    
     @IBOutlet weak var tableView: UITableView!
+
+    
+    
     
     @IBOutlet weak var bigTitle: UILabel!
-    override func viewDidLoad() {
+    
+    
+    //part 8
+    
+    var categoryOneImagesData = [String]()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+            //part 8
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+                        let dict = NSDictionary(contentsOfFile: path!)
+                        categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
+
+
         
         // Do any additional setup after loading the view.
         
---
+++ Halloween And Holiday Candy App Review/secondDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  secondDetailViewController.swift
+//  Halloween And Holiday Candy App Review
+//
+//  Created by Yuki Decker on 11/2/23.
+//
+
+import UIKit
+
+class secondDetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePass: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let imageName = imagePass {
+            imageView.image = UIImage(named: imageName)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
